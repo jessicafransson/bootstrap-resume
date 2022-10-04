6cb41ac0c330def0992cd905fc5656a584ea59ce
Jessica Fransson <jessicafransson@outlook.com>
2022-10-04 07:35:55 +0000

    changed displayed error message to api

--- assets/js/github-information.js
+++ assets/js/github-information.js
@@ -37,7 +37,7 @@ function repoInformationHTML(repos) {
 }
 
 function fetchGitHubInformation(event) {
-    $("gh-user-data").html("");
+    $("#gh-user-data").html("");
     $("#gh-repo-data").html("");
 
     var username = $("#gh-username").val();
@@ -65,6 +65,9 @@ function fetchGitHubInformation(event) {
             if (errorResponse.status === 404) {
                 $("#gh-user-data").html(
                     `<h2>No info found for user ${username}</h2>`);
+            } else if (errorResponse.status === 403) {
+                var resetTime = new Date(errorResponse.getResponseHeader('X-RateLimit-Reset') * 1000);
+                $("#gh-user-data").html(`<h4>Too many requests, please wait until ${resetTime.toLocaleTimeString()}</h4>`);
             } else {
                 console.log(errorResponse);
                 $("#gh-user-data").html(
